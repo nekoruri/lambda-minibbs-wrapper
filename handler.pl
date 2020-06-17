919a0c3d32881934dcf956fa9063ec6abd1f5001
NAKAYAMA Masahiro <aki@nekoruri.jp>
2020-06-17 15:26:08 +0900

    do not use App.pm

--- handler.pl
+++ handler.pl
@@ -3,8 +3,6 @@ use warnings;
 use strict;
 use AWS::Lambda::PSGI;
 use Plack::App::WrapCGI;
-use lib $ENV{'LAMBDA_TASK_ROOT'};
-use App;
 
 my $app = Plack::App::WrapCGI->new(script => "$ENV{'LAMBDA_TASK_ROOT'}/minibbs.cgi", execute => 1)->to_app;
 my $func = AWS::Lambda::PSGI->wrap($app);
