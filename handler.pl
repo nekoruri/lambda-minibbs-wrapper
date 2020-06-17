35a257c4e6941fac2015c79f9dec4c1d15d65d46
NAKAYAMA Masahiro <aki@nekoruri.jp>
2020-06-17 14:08:35 +0900

    remove comment

--- handler.pl
+++ handler.pl
@@ -6,7 +6,6 @@ use Plack::App::WrapCGI;
 use lib $ENV{'LAMBDA_TASK_ROOT'};
 use App;
 
-#my $app = Plack::App::WrapCGI->new(script => "$ENV{'LAMBDA_TASK_ROOT'}/minibbs.cgi", execute => 1)->to_app;
 my $app = App->new(script => "$ENV{'LAMBDA_TASK_ROOT'}/minibbs.cgi", execute => 1)->to_app;
 my $func = AWS::Lambda::PSGI->wrap($app);
 
